a4ce153250c5e6fbd0cab80b7da6ac3041ed42db
GRANDSOFT\wanxh <wanxh@glodon.com>
2022-08-12 18:56:05 +0800

    测试demo

--- rpc-test/src/main/java/cn/wanxh/test/Teacher.java
+++ rpc-test/src/main/java/cn/wanxh/test/Teacher.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class Teacher implements FactoryBean {
     @Override
     public Object getObject() throws Exception {
-        return new UserInfo(); // 不被Spring容器管理，容器中也就不存UserInfo对应的BeanDefinition信息
+        return new UserInfo(); // 不被Spring容器管理？，容器中也就不存UserInfo对应的BeanDefinition信息。生命周期简化，没有复杂的实例化和属性注入的过程，可以在初始化前后对其进行扩展
     }
 
     @Override
